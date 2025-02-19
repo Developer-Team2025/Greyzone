fe42d53d9d074bb1522f03f22163c941e4cd8566
Joben-dev <jobendev6789@gmail.com>
2025-02-19 15:11:39 +0800

    update the frontend to adjust on the backend

--- src/Pages/Certificate/index.tsx
+++ src/Pages/Certificate/index.tsx
@@ -1,71 +1,81 @@
 
+import { useEffect, useState } from 'react';
 import styles from './style.module.scss'
 import classNames from "classnames";
-const certificates = [
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/benjamin.jpg`,
-    "alt": "benjamin",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/benjamin_acad.jpg`,
-    "alt": "benjamin_acad",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Amanda_acad.jpg`,
-    "alt": "Amanda",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Amanda_inves.jpg`,
-    "alt": "Amanda_inves",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Gabriella.jpg`,
-    "alt": "Gabriella",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/jasmine_valentine.jpg`,
-    "alt": "Jasmine",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Jessica.jpg`,
-    "alt": "Jessica",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/John.jpg`,
-    "alt": "John",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Jon.jpg`,
-    "alt": "jon",
-    "caption": ""
-  },	
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Joseph.jpg`,
-    "alt": "Joseph",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Phoebe.jpg`,
-    "alt": "Phoebe",
-    "caption": ""
-  },
-  {
-    "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Richard.jpg`,
-    "alt": "Richard",
-    "caption": ""
-  }
-]
+import { formApi } from '../../Components/api/getValue';
+// const certificates = [
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/benjamin.jpg`,
+//     "alt": "benjamin",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/benjamin_acad.jpg`,
+//     "alt": "benjamin_acad",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Amanda_acad.jpg`,
+//     "alt": "Amanda",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Amanda_inves.jpg`,
+//     "alt": "Amanda_inves",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Gabriella.jpg`,
+//     "alt": "Gabriella",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/jasmine_valentine.jpg`,
+//     "alt": "Jasmine",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Jessica.jpg`,
+//     "alt": "Jessica",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/John.jpg`,
+//     "alt": "John",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Jon.jpg`,
+//     "alt": "jon",
+//     "caption": ""
+//   },	
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Joseph.jpg`,
+//     "alt": "Joseph",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Phoebe.jpg`,
+//     "alt": "Phoebe",
+//     "caption": ""
+//   },
+//   {
+//     "src": `${import.meta.env.VITE_REDIRECT}/img/certificate/Richard.jpg`,
+//     "alt": "Richard",
+//     "caption": ""
+//   }
+
+// ]
 
 
 const index = () => {
+  const [ourteam, setTEAM] = useState([])
+  useEffect(()=>{
+    formApi('certificate', {}).then(res=>{
+      setTEAM(res)
+    })
+  },[])
+  
   return (
     <main>
       <section className={styles.privacy} id="privacy">
@@ -78,7 +88,7 @@ const index = () => {
               Our team is composed of some very talented professionals. They are people, who are always prepared for new challenges, capable to provide our customers with the best quality work. Our team is one of the most important assets, which helps us to improve our products on a daily basis.
               </p>
               <div className={classNames(styles.certificate_gallery,` grid gap-8`)} >
-                {certificates.map((certificate, index) => (
+                {ourteam && ourteam?.map((certificate:any, index: number) => (
                     <figure key={index} className="certificate-item">
                     <img
                         src={certificate.src}
--- src/Pages/Digital/index.tsx
+++ src/Pages/Digital/index.tsx
@@ -1,11 +1,11 @@
 import ShadowDOM from 'react-shadow';
-import styles from '../../themes.module.scss'
+
 const index = () => {
   return (
     <ShadowDOM.div>
     <style>
-        {/* {`
-            @import url(https://tandhconsult.com/wp-content/themes/th/styles.css);
+        {`
+            @import url(https://tandhconsult.com/wp-content/themes/th/style.css);
             .slick-track{
             gap: 1rem;
             }
@@ -15,84 +15,116 @@ const index = () => {
                 display: inline-block;
                 background: url(https://tandhconsult.com/wp-content/themes/th/img/icons/time-management.svg);
             }
-        `} */}
+        `}
     </style>
-        <section className={styles.solutions} id="solutions">
-        <div className={styles.container}>
-            <h2 className={`${styles.title} ${styles.solutions__title} wow animate__animated animate__fadeInLeft`}>
-            <span>Digital Goods</span>
-            </h2>
-            <div className={`${styles.dispute__info} wow animate__animated animate__fadeInLeft`}>
-            <p>
-                Explore our range of digital products designed to enhance your knowledge and skills in fraud prevention and online security...
-            </p>
-            </div>
-            
-            <div className={`${styles.dispute__wrapper} wow animate__animated-delay`}>
-            {[{
-                href: "/solutions/digital-goods/10-indicators-of-a-love-scam-a-comprehensive-checklist",
-                imgSrc: "indicators.jpg",
-                name: "10 Indicators Of A Love Scam: A Comprehensive Checklist"
-            }, {
-                href: "/solutions/digital-goods/online-fraud-protection-a-practical-guide",
-                imgSrc: "online_fraud.jpg",
-                name: "Online Fraud Protection: A Practical Guide"
-            }, {
-                href: "/solutions/digital-goods/printable-fraud-awareness-workbook-with-exercises",
-                imgSrc: "printable.jpg",
-                name: "Printable Fraud Awareness Workbook with Exercises"
-            }, {
-                href: "/solutions/digital-goods/fraud-awareness-masterclass-with-detailed-presentation",
-                imgSrc: "masterclass.jpg",
-                name: "Fraud Awareness Masterclass with Detailed Presentation"
-            }].map((item, index) => (
-                <a key={index} className={`${styles.dispute__link} wow animate__animated animate__fadeInUp`} href={item.href}>
-                <picture className={styles.dispute__pic}>
-                    <img src={`${import.meta.env.VITE_REDIRECT}/img/${item.imgSrc}`} alt={item.imgSrc.split('.')[0]} />
-                </picture>
-                <span className={styles.dispute__name}>{item.name}</span>
-                <span className={styles.dispute__more}>See more<i className="i i-arrow-right"></i></span>
+        <section className="solutions" id="solutions">
+            <div className="container">
+                <h2 className="title solutions__title wow animate__animated animate__fadeInLeft" >
+                    <span>Digital Goods</span>
+                </h2>
+                <div className="dispute__info wow animate__animated animate__fadeInLeft" >
+                    <p>
+                        Explore our range of digital products designed to enhance your knowledge and skills in fraud prevention and online security. Our offerings include comprehensive workbooks, detailed self-assessments, interactive exercises, and curated resources that provide valuable insights and practical tools for staying vigilant against scams. Each product is crafted to help you improve your fraud detection capabilities, implement effective security practices, and stay informed about the latest fraud prevention techniques. Perfect for individuals and professionals looking to boost their cybersecurity measures and protect themselves from fraudulent activities.
+                    </p>
+                </div>
+
+                <div className="dispute__wrapper wow animate__animated-delay">
+                <a
+                    className="dispute__link wow animate__animated animate__fadeInUp"
+                    
+                    href="/solutions/digital-goods/10-indicators-of-a-love-scam-a-comprehensive-checklist"
+                >
+                    <picture className="dispute__pic">
+                    <img src={`${import.meta.env.VITE_REDIRECT}/img/indicators.jpg`} alt="indicators" />
+                    </picture>
+                    <span className="dispute__name">10 Indicators Of A Love Scam: A Comprehensive Checklist</span>
+                    <span className="dispute__more">See more<i className="i i-arrow-right"></i></span>
                 </a>
-            ))}
-            </div>
-            
-            <div className={`${styles.ePagination} wow animate__animated fadeInUp cn-mt-100`}></div>
-            
-            <h2 className={`${styles.title} ${styles.dispute__subtitle} wow animate__animated animate__fadeInUp`}>
-            <span>Benefits of working with us</span>
-            </h2>
-            <div className={`${styles.dispute__subinfo} wow animate__animated animate__fadeInUp`}>
-            <p>“Highly qualified staff”...</p>
-            </div>
-            
-            <div className={styles.dispute__benef}>
-            {[{
-                icon: "time-management",
-                name: "Expert Help",
-                desc: "You can now get expert help across a wide range of areas – and help is at hand as soon as you need it."
-            }, {
-                icon: "help.svg",
-                name: "Establish Professional Credibility",
-                desc: "A relationship built on trust and honesty can strengthen any business."
-            }, {
-                icon: "help.svg",
-                name: "Faster, Easier Implementation",
-                desc: "Faster, easier and more cost-effective than any other provider."
-            }, {
-                icon: "shield.svg",
-                name: "Build Trust",
-                desc: "We are here for you – because we are Built to Care."
-            }].map((item, index) => (
-                <div key={index} className={`${styles.dispute__benef_item} wow animate__animated animate__fadeInUp`}>
-                <i className={`i i-${item.icon}`} style={item.icon.includes('.svg') ? {maskImage: `url(https://tandhconsult.com/wp-content/uploads/2021/05/${item.icon})`, background: `url(https://tandhconsult.com/wp-content/uploads/2021/05/${item.icon})`} : {}}></i>
-                <div className={styles.dispute__benef_content}>
-                    <span className={styles.dispute__benef_name}>{item.name}</span>
-                    <span className={styles.dispute__benef_pos}>{item.desc}</span>
+
+                <a
+                    className="dispute__link wow animate__animated animate__fadeInUp"
+                    
+                    href="/solutions/digital-goods/online-fraud-protection-a-practical-guide"
+                >
+                    <picture className="dispute__pic">
+                    <img src={`${import.meta.env.VITE_REDIRECT}/img/online_fraud.jpg`} alt="online_fraud" />
+                    </picture>
+                    <span className="dispute__name">Online Fraud Protection: A Practical Guide</span>
+                    <span className="dispute__more">See more<i className="i i-arrow-right"></i></span>
+                </a>
+
+                <a
+                    className="dispute__link wow animate__animated animate__fadeInUp"
+                    
+                    href="/solutions/digital-goods/printable-fraud-awareness-workbook-with-exercises"
+                >
+                    <picture className="dispute__pic">
+                    <img src={`${import.meta.env.VITE_REDIRECT}/img/printable.jpg`} alt="printable" />
+                    </picture>
+                    <span className="dispute__name">Printable Fraud Awareness Workbook with Exercises</span>
+                    <span className="dispute__more">See more<i className="i i-arrow-right"></i></span>
+                </a>
+
+                <a
+                    className="dispute__link wow animate__animated animate__fadeInUp"
+                    
+                    href="/solutions/digital-goods/fraud-awareness-masterclass-with-detailed-presentation"
+                >
+                    <picture className="dispute__pic">
+                    <img src={`${import.meta.env.VITE_REDIRECT}/img/masterclass.jpg`} alt="masterclass" />
+                    </picture>
+                    <span className="dispute__name">Fraud Awareness Masterclass with Detailed Presentation</span>
+                    <span className="dispute__more">See more<i className="i i-arrow-right"></i></span>
+                </a>
                 </div>
+
+                <div className="e-pagination wow animate__animated fadeInUp cn-mt-100"></div>
+
+                <h2 className="title dispute__subtitle wow animate__animated animate__fadeInUp">
+                <span>Benefits of working with us</span>
+                </h2>
+                <div className="dispute__subinfo wow animate__animated animate__fadeInUp" >
+                <p>“Highly qualified staff”, our staff that has been working with cases of individuals and businesses on the daily basis for many years now, therefore, it enables them to swiftly understand and identify their needs and give offer them tailor-made solutions.</p>
+                <p>Besides, the certifications of our staff provide with necessary assurance and surety that you are in good hands and persons that you are dealing with are certified professionals with a proven track record and excellent reputation which in most cases is paramount.</p>
+                <p>Professional certifications can also help give your independent business a solid foundation. The advanced training, information, and knowledge you gain from specialized coursework can provide you with up-to-date tools and technical strategies that will serve to guide and direct you in the execution of your projects, allowing you to manage all aspects of your work more effectively.</p>
+                <p>In order to increase earning potential, solving the complex blockchain problems and investigating the prompt ways with the industry-leading tool is a huge asset, let alone it being the trend of the world that we live in.</p>
+                <p>The biggest challenge for the majority of people is the understanding of the crypto world and its dynamics, just like any new major adaptation it will take its own time, and thereby organizations and individuals are pacing up to adapt and partake in the revolutionary transformation of cryptocurrencies in order to stay updated and meet current demands of the market.</p>
                 </div>
-            ))}
+
+                <div className="dispute__benef" >
+                    <div className="dispute__benef-item wow animate__animated animate__fadeInUp" >
+                        <i className="i i-time-management"></i>
+                        <div className="dispute__benef-content">
+                        <span className="dispute__benef-name">Expert Help</span>
+                        <span className="dispute__benef-pos">You can now get expert help across a wide range of areas – and help is at hand as soon as you need it.</span>
+                        </div>
+                    </div>
+
+                    <div className="dispute__benef-item wow animate__animated animate__fadeInUp" >
+                        <i className="i i-time-management" style={{maskImage:'url(https://tandhconsult.com/wp-content/uploads/2021/05/help.svg)', background: "url(https://tandhconsult.com/wp-content/uploads/2021/05/help.svg)"}}></i>
+                        <div className="dispute__benef-content">
+                        <span className="dispute__benef-name">Establish Professional Credibility</span>
+                        <span className="dispute__benef-pos">A relationship built on trust and honesty can strengthen any business.</span>
+                        </div>
+                    </div>
+
+                    <div className="dispute__benef-item wow animate__animated animate__fadeInUp" >
+                        <i className="i i-time-management" style={{maskImage:'url(https://tandhconsult.com/wp-content/uploads/2021/05/help.svg)', background: "url(https://tandhconsult.com/wp-content/uploads/2021/05/help.svg)"}}></i>
+                        <div className="dispute__benef-content">
+                        <span className="dispute__benef-name">Faster, Easier Implementation</span>
+                        <span className="dispute__benef-pos">Faster, easier and more cost-effective than any other provider.</span>
+                        </div>
+                    </div>
+
+                    <div className="dispute__benef-item wow animate__animated animate__fadeInUp" >
+                        <i className="i i-time-management" style={{maskImage:'url(https://tandhconsult.com/wp-content/uploads/2021/05/shield.svg)', background: "url(https://tandhconsult.com/wp-content/uploads/2021/05/shield.svg)"}}></i>
+                        <div className="dispute__benef-content">
+                        <span className="dispute__benef-name">Build Trust</span>
+                        <span className="dispute__benef-pos">We are here for you – because we are Built to Care.</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         </section>
     </ShadowDOM.div>
   )
